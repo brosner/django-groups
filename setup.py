8323ebf05a6ed3b81a707e1ca79a2aac81b49560
Brian Rosner <brosner@gmail.com>
2009-07-07 17:05:32 -0600

    added a setup.py.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+setup(
+    name = "django-groups",
+    version = "0.0",
+    url = "http://pinaxproject.com/docs/dev/groups.html",
+    description = "Django group support (extracted from Pinax)",
+    long_description = open("README", "rb").read(),
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
