6a5c20b398b5ca268c41211a4bbd99d8f6fef82f
Brian Rosner <brosner@gmail.com>
2010-03-29 00:02:41 -0600

    0.1.dev10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "django-groups",
-    version = "0.1.dev9",
+    version = "0.1.dev10",
     url = "http://pinaxproject.com/docs/dev/groups.html",
     description = "Django group support (extracted from Pinax)",
     long_description = open("README", "rb").read(),
